bf438387ac8fba91f7348e9dab639554ee8dba63
Wong Liang Zan <zan@liangzan.net>
2016-06-19 17:28:34 +0800

    getting the exchange rates from api

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,13 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE TemplateHaskell, LambdaCase, OverloadedStrings #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Lib where
+
+import Data.Aeson
+import Network.Wreq
+
+apiUrl = "http://www.apilayer.net/api/live?access_key=a4bb8f00567594307836da145bfb5748&format=1"
+
+getExchangeRates :: IO ()
+getExchangeRates = do
+  rates <- get apiUrl
+  print rates
